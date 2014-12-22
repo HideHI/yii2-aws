23f8f30cd0009cd0e240c1d18320d50e6ef90a13
James Brooking <jambroo@gmail.com>
2014-12-22 22:21:41 +0100

    Abstract link helper.

---
+++ view/helper/AbstractLinkHelper.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace jambroo\aws\view\helper;
+
+use Yii;
+
+use jambroo\aws\view\exception\InvalidSchemeException;
+
+/**
+ * Common functionality for link generation.
+ *
+ * @author James Brooking <jambroo@gmail.com>
+ * @since 1.0
+ */
+class AbstractLinkHelper
+{
+    /**
+     * @var string
+     */
+    protected $scheme = 'https';
+
+    /**
+     * @var array
+     */
+    protected $supportedSchemes = array('http', 'https', null);
+
+    /**
+     * Set if HTTPS should be used for generating URLs
+     *
+     * @param bool $useSsl
+     *
+     * @return self
+     * @deprecated
+     */
+    public function setUseSsl($useSsl)
+    {
+        $this->setScheme($useSsl ? 'https' : 'http');
+
+        return $this;
+    }
+
+    /**
+     * Get if HTTPS should be used for generating URLs
+     *
+     * @return bool
+     * @deprecated
+     */
+    public function getUseSsl()
+    {
+        return $this->getScheme() === 'https';
+    }
+
+    /**
+     * Set the scheme to use for generating URLs.  Supported schemes
+     * are http, https and null (see {@link $supportedSchemes}).
+     *
+     * @param string $scheme
+     * @throws InvalidSchemeException
+     * @return self
+     */
+    public function setScheme($scheme)
+    {
+        if (!in_array($scheme, $this->supportedSchemes, true)) {
+            $schemes = implode(', ', $this->supportedSchemes);
+
+            throw new InvalidSchemeException('Schemes must be one of ' . $schemes);
+        }
+
+        $this->scheme = $scheme;
+
+        return $this;
+    }
+
+    /**
+     * Get the scheme to be used for generating URLs
+     *
+     * @return string
+     */
+    public function getScheme()
+    {
+        return $this->scheme;
+    }
+}
