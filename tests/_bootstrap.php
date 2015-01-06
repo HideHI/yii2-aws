7569aa1f80f8df4215f1916c1273f87731fd7f5d
James Brooking <jambroo@gmail.com>
2015-01-06 22:43:33 +0100

    Added code to be able to access Yii from tests. Should review later.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,9 @@
 <?php
 // This is global bootstrap for autoloading
+// This seems incorrect to have to load these classes with so many parent directories
+require_once(__DIR__ . '/../../../../vendor/autoload.php');
+require_once(__DIR__ . '/../../../../vendor/yiisoft/yii2/Yii.php');
+
+$config = require(__DIR__ . '/../../../../config/web.php');
+
+(new yii\web\Application($config));
