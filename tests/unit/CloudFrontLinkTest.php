31e4865efd67a73ac188c8e06a9b1a264f97a154
James Brooking <jambroo@gmail.com>
2015-01-16 21:06:43 +0000

    Rest of CloudFrontLink tests.

--- tests/unit/CloudFrontLinkTest.php
+++ tests/unit/CloudFrontLinkTest.php
@@ -22,12 +22,12 @@ class CloudFrontLinkTest extends \Codeception\TestCase\Test
         ));
         $this->viewHelper = new CloudFrontLink($this->cloudFrontClient);
     }
-    
+
     public function testAssertDoesUseSslByDefault()
     {
         $this->assertTrue($this->viewHelper->getUseSsl());
     }
-    
+
     /**
      * @expectedException \jambroo\aws\view\exception\InvalidSchemeException
      */
@@ -93,5 +93,43 @@ class CloudFrontLinkTest extends \Codeception\TestCase\Test
     {
         $this->viewHelper->setDefaultDomain('');
         $link = $this->viewHelper->__invoke('my-object');
+    }
+
+    public function testGenerateSignedLink()
+    {
+        if (!extension_loaded('openssl')) {
+            $this->markTestSkipped('OpenSSL is required for this test.');
+        }
+        $pemFile = sys_get_temp_dir() . '/aws-sdk-php-zf2-cloudfront-test.pem';
+        if (!file_exists($pemFile)) {
+            // Generate a new Certificate Signing Request and public/private keypair
+            $csr = openssl_csr_new(array(), $keypair);
+            // Create a self-signed certificate
+            $x509 = openssl_csr_sign($csr, null, $keypair, 1);
+            openssl_x509_export($x509, $certificate);
+            // Create and save a private key
+            $privateKey = openssl_get_privatekey($keypair);
+            openssl_pkey_export_to_file($privateKey, $pemFile);
+        }
+        $clientConfig = $this->cloudFrontClient->getConfig();
+        $clientConfig->set('key_pair_id', 'kpid');
+        $clientConfig->set('private_key', $pemFile);
+        $this->viewHelper->setHostname('example.com');
+        $link = $this->viewHelper->__invoke('my-object', '123abc', time() + 600);
+        $this->assertRegExp(
+            '#^https\:\/\/123abc\.example\.com\/my-object\?Expires\=(.*)\&Signature\=(.*)\&Key-Pair-Id\=kpid$#',
+            $link
+        );
+    }
+    
+    /**
+     * @expectedException \jambroo\aws\view\exception\InvalidSchemeException
+     */
+    public function testGenerateSignedProtocolRelativeLink()
+    {
+        $this->viewHelper
+            ->setHostname('example.com')
+            ->setScheme(null);
+        $link = $this->viewHelper->__invoke('my-object', '123abc', time() + 600);
     }
 }
